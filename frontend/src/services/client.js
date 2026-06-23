af441eda5514763370bbbf438f178f57b397cf0c
AnkitBind21 <bscit.ankitbind@gmail.com>
2026-06-23 19:07:28 +0530

    Connect frontend to Render backend

--- frontend/src/services/client.js
+++ frontend/src/services/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://wcgan-web-interface.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
